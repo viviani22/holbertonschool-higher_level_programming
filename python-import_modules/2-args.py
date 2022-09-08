2c5358eb88bee8730292f07aaf46116ed1d60409
viviani22 <vivianxhumetiku@gmail.com>
2022-09-08 02:15:19 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,10 +2,12 @@
 if __name__ == "__main__":
     import sys
     i = 1
-    if (len(sys.argv) > 1):
+    if (len(sys.argv) == 2):
+        print("{} argument:".format(len(sys.argv) - 1))
+    elif (len(sys.argv) > 2):
         print("{} arguments:".format(len(sys.argv) - 1))
     else:
-        print("0 argument.")
+        print("0 arguments.")
     while i != len(sys.argv):
         print("{}: {}".format(i, str(sys.argv[i])))
         i += 1
