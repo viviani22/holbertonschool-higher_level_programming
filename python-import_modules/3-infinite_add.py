cef89d8acea7f1bd9d8ba407253432528e1bc56f
viviani22 <vivianxhumetiku@gmail.com>
2022-09-08 02:16:36 -0700

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-i = 1
-sum_arg = 0
-while i != len(sys.argv):
-    if sys.argv[i].isnumeric():
-        sum_arg += int(sys.argv[i])
-    i += 1
-print("{}".format(sum_arg))
+if __name__ == "__main__":
+    import sys
+    i = 1
+    sum_arg = 0
+    while i != len(sys.argv):
+        if sys.argv[i].isnumeric():
+            sum_arg += int(sys.argv[i])
+        i += 1
+    print("{}".format(sum_arg))
