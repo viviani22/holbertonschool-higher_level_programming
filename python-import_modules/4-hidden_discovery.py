28a29962fff7cca04e0f68a23caae9cfa6771da8
viviani22 <vivianxhumetiku@gmail.com>
2022-09-09 00:38:20 -0700

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-for strings in dir(hidden_4):
-    if "__" not in strings:
-        print(strings)
+if __name__ == "__main__":
+    import hidden_4
+    for strings in dir(hidden_4):
+            if "__" not in strings:
+                print(strings)
