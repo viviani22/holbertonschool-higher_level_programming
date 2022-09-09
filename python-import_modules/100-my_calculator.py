22c2e5730162c70a7555c5aefe99e30f83b0e463
viviani22 <vivianxhumetiku@gmail.com>
2022-09-09 02:47:16 -0700

    task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,11 +2,13 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+    """
     if (len(sys.argv) != 3):
         exit(1)
-    a = int(str(sys.argv[1]))
-    op = str(sys.argv[2])
-    b = int(str(sys.argv[3]))
+    """
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
     if (op == "+"):
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif (op == "-"):
