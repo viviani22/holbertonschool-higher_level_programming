f36beb8547e004ae1ea0eead9825c3f6b3265158
viviani22 <vivianxhumetiku@gmail.com>
2022-09-12 01:31:13 -0700

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
-        return None
+        return my_list 
     for number in range(len(my_list)):
         if number == idx:
             my_list[number] = element
