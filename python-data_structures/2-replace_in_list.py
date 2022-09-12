fc9ea90b50fda301b730b1706b949361f1209888
viviani22 <vivianxhumetiku@gmail.com>
2022-09-12 01:24:53 -0700

    task 2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return None
+    for number in range(len(my_list)):
+        if number == idx:
+            my_list[number] = element
+            return my_list
