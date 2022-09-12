456ae2e19a863e00ea20e0ad8aeb79099952417c
viviani22 <vivianxhumetiku@gmail.com>
2022-09-12 02:56:27 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for element in matrix:
             for number in element:
-                print("{}".format(number), end="")
+                print("{:d}".format(number), end="")
                 if number != element[-1]:
                     print(end=" ")
             print()
