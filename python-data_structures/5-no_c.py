4e2abb9673f993f4188b33bbeb009c6d8d200e5b
viviani22 <vivianxhumetiku@gmail.com>
2022-09-12 01:59:43 -0700

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     if my_string:
-        new_string = ""
         for letter in my_string:
             if letter == 'c' or letter == 'C':
                 continue
