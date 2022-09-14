981b48617308e20219acef8943b9336d06946d52
viviani22 <vivianxhumetiku@gmail.com>
2022-09-14 01:36:30 -0700

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'IV': 4, 'IX': 9, 'XL': 40,
+            'XC': 90, 'CD': 400}
+    if not type(roman_string) == str or roman_string is None:
+        return 0
+    i = 0
+    decimal_number = 0
+    while i < len(roman_string):
+        if roman_string[i:i+2] in roman_dictionary:
+            decimal_number += roman_dictionary[roman_string[i:i+2]]
+            i += 2
+        elif roman_string[i] in roman_dictionary:
+            decimal_number += roman_dictionary[roman_string[i]]
+            i += 1
+    return decimal_number
