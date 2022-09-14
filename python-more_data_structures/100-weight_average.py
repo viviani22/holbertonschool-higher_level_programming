bca0792d1f1f7ae8c9231f8f9f56d1ccc7ed5fb1
viviani22 <vivianxhumetiku@gmail.com>
2022-09-14 03:37:59 -0700

    task 13

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     weighted_sum = 0
     divider = 0
     for pair in my_list:
         divider += pair[1]
         weighted_sum += (pair[0] * pair[1])
     return weighted_sum / divider
-
