4fcef8867fd6d4e37ac22c9af56ceeb7c9a9a379
viviani22 <vivianxhumetiku@gmail.com>
2022-09-16 04:37:30 -0700

    task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            new_list.append(a/b)
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+        finally:
+            continue
+    return new_list
