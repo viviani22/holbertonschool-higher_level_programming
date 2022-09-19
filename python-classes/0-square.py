ba7c713dfb26d023a742f3ebe30d42a1e5f05515
viviani22 <vivianxhumetiku@gmail.com>
2022-09-19 03:13:10 -0700

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 class Square:
-    """Class named Square
-
-    Args:
-        None as of now
-
-    Returns:
-        Reference to class
-
-    """
+    """Class named Square"""
