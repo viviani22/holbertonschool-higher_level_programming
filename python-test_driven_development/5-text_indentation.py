dcd1866324fd3919d9c750460178c3ba61102484
viviani22 <vivianxhumetiku@gmail.com>
2022-09-21 01:29:54 -0700

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,8 +7,11 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     indented_text = ""
     for i in range(len(text)):
-        if text[i] == ' ' and text[i-1] in ['.', '?', ':']:
+        if text[i] in ['.', '?', ':']:
+            indented_text += text[i]
             indented_text += '\n' + '\n'
+            continue
+        if text[i] == ' ' and text[i-1] in ['.', '?', ':']:
             continue
         indented_text += text[i]
     print(indented_text)
