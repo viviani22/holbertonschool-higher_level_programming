e0dd95f96ecb093fc375e25083f3f37d44e5db3f
viviani22 <vivianxhumetiku@gmail.com>
2022-09-21 02:00:24 -0700

    fix task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Task 3"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Prints first and last name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
