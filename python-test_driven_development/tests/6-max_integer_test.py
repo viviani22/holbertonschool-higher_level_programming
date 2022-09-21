e4ac5df0a6cc76a928d5014793bc933c7bb510cb
viviani22 <vivianxhumetiku@gmail.com>
2022-09-21 04:02:28 -0700

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,10 +9,9 @@ class TestMaxInteger(unittest.TestCase):
     """Class for testing function"""
     def test_max(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        self.assertEqual(max_integer([float('inf'), 2, 3]), inf)
-
-
-    def test_types(self):
-        self.assertRaises(TypeError, max_integer, [1, 'sup'])
-        self.assertRaises(TypeError, max_integer, [float('NaN'), 'sup'])
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1, 4, 3, 1]), 4)
+        self.assertEqual(max_integer([1, 4, -3, 1]), 4)
+        self.assertEqual(max_integer([-1, -4, -3, -1]), -1)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)
