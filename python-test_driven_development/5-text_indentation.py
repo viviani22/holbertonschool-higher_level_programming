a54c13c4f48dc73352c377fd457cc7a255054541
viviani22 <vivianxhumetiku@gmail.com>
2022-09-21 01:23:45 -0700

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,6 +10,6 @@ def text_indentation(text):
         if text[i] == ' ' and text[i-1] in ['.', '?', ':']:
             continue
         indented_text += text[i]
-        if text[i] in ['.', '?', ':']:
+        if text[i] in ['.', '?', ':'] and text[i+1] == ' ':
             indented_text += '\n' + '\n'
     print(indented_text)
