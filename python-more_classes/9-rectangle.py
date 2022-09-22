34b3db277f7fcab54027ad3eb616e0fc1196960d
viviani22 <vivianxhumetiku@gmail.com>
2022-09-22 04:15:01 -0700

    task 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -9,16 +9,16 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
 
     @property
     def height(self):
@@ -80,4 +80,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        if type(size) is not int:
+            raise TypeError()
         return cls(size, size)
