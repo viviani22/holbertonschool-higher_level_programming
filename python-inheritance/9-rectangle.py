556154e5b683e3027cbb7d028d4cec432d0f44cf
viviani22 <vivianxhumetiku@gmail.com>
2022-09-28 01:01:27 -0700

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle(bg):
         return self.__width * self.__height
 
     def __repr__(self):
-        print (f"[Rectangle] {self.__width}/{self.__height}")
+        print(f"[Rectangle] {self.__width}/{self.__height}")
 
     def __str__(self):
         return (f"[Rectangle] {self.__width}/{self.__height}")
