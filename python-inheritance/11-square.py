d6d67e0b098f7fa472a2f8d73ede066bbc9471c2
viviani22 <vivianxhumetiku@gmail.com>
2022-09-28 02:16:13 -0700

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task 10"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __repr__(self):
+        print(f"[Square] {self.__size}/{self.__size}")
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
