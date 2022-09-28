c4f891a86d37af60ae63fd1b238919f8d25206f8
viviani22 <vivianxhumetiku@gmail.com>
2022-09-28 01:27:54 -0700

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task 10"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class"""
+    def __init__(self, size):
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
