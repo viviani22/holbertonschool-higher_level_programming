2e8fa598646472974e1f8d568e3b080312aa8783
viviani22 <vivianxhumetiku@gmail.com>
2022-09-28 01:00:50 -0700

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task 7"""
+bg = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(bg):
+    """Class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __repr__(self):
+        print (f"[Rectangle] {self.__width}/{self.__height}")
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
