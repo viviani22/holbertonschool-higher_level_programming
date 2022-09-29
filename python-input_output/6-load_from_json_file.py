fec7b6f97e11f4b5ab0bf811fb9219521b02294d
viviani22 <vivianxhumetiku@gmail.com>
2022-09-29 03:34:49 -0700

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,5 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
+    """loads object from json file"""
     with open(filename, encoding="utf-8") as file:
         return json.loads(file.readline())
