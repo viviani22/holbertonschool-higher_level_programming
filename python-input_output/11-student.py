c3523768a55bc14bf8f193fee710fc12184036bc
viviani22 <vivianxhumetiku@gmail.com>
2022-09-30 07:46:53 -0700

    task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -21,6 +21,6 @@ class Student:
         return dictionary
 
     def reload_from_json(self, json):
-        self.first_name = json[first_name]
-        self.last_name = json[last_name]
-        self.age = json[age]
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
