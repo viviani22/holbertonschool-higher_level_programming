631899276f94b1e0b154add8e35ebe92b60ab4fb
viviani22 <vivianxhumetiku@gmail.com>
2022-09-30 07:44:58 -0700

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Task 9"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        if "first_name" in attrs:
+            dictionary["first_name"] = self.first_name
+        if "last_name" in attrs:
+            dictionary["last_name"] = self.last_name
+        if "age" in attrs:
+            dictionary["age"] = self.age
+        return dictionary
+
+    def reload_from_json(self, json):
+        self.first_name = json[first_name]
+        self.last_name = json[last_name]
+        self.age = json[age]
