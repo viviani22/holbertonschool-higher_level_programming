aa674e72b79032ce534894dc3b39552412923669
viviani22 <vivianxhumetiku@gmail.com>
2022-09-30 07:07:06 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """task 7"""
 import sys
+import os
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arguments = sys.argv[1:]
+old_arg = []
+if os.path.exists("add_item.json"):
+    old_arg = load_from_json_file("add_item.json")
+arguments = old_arg + sys.argv[1:]
 save_to_json_file(arguments, "add_item.json")
-load_from_json_file("add_item.json")
