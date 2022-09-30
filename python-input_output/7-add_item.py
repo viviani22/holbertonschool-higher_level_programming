5447beadc3d14551b73dfb381a43c7267fd3d855
viviani22 <vivianxhumetiku@gmail.com>
2022-09-30 06:47:48 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,4 +6,4 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv[1:]
 save_to_json_file(arguments, "add_item.json")
-print(load_from_json_file("add_item.json"))
+load_from_json_file("add_item.json")
