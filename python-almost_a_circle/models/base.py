ab8a9457877462bbaac2261e884582b1898379c2
viviani22 <vivianxhumetiku@gmail.com>
2022-10-03 01:40:00 -0700

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Task 1"""
+
+
+class Base:
+    """class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = id
