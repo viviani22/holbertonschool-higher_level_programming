b7a2fa9c5703a57b622fc7bac8b2bab793838201
viviani22 <vivianxhumetiku@gmail.com>
2022-10-04 00:57:11 -0700

    task 16

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -111,3 +111,8 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height,
+                'width': self.width}
