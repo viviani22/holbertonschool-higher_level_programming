597d6130349848f4ef2c2fe7bc4638e5d91051ba
viviani22 <vivianxhumetiku@gmail.com>
2022-10-06 10:56:14 -0700

    test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,8 @@
 import unittest
 from models.rectangle import Rectangle
+import os.path
+import io 
+import sys
 
 class TestBase(unittest.TestCase):
     def test_rectangle_assignments(self):
@@ -36,10 +39,79 @@ class TestBase(unittest.TestCase):
     def test_rectangle_str(self):
         r1 = Rectangle(1, 1, 1, 1, 1)
         self.assertEqual(r1.__str__(), "[Rectangle] (1) 1/1 - 1/1")
+
     def test_rectangle_display(self):
-        r1 = Rectangle(1, 1)
-        self.assertEqual(r1.display(), None)
+        r1 = Rectangle(1, 1, 1, 1)
+        output = io.StringIO()
+        sys.stdout = output
+        r1.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "\n #\n")
         r2 = Rectangle(1, 1, 1)
-        self.assertEqual(r2.display(), None)
-        r3 = Rectangle(1, 1, 1, 1)
-        self.assertEqual(r3.display(), None)
+        output = io.StringIO()
+        sys.stdout = output
+        r2.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), " #\n")
+        r3 = Rectangle(1, 1)
+        output = io.StringIO()
+        sys.stdout = output
+        r3.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), "#\n")
+    def test_rectangle_to_dictionary(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        self.assertEqual(r1.to_dictionary(), {'x': 1, 'y': 1, 'id': 1, 'height': 1, 'width': 1})
+    def test_rectangle_update(self):
+        r1 = Rectangle(1, 1)
+        r1.update(89)
+        self.assertEqual(r1.id, 89)
+        r1.update(89, 1)
+        self.assertEqual(r1.width, 1)
+        r1.update(89, 1, 2)
+        self.assertEqual(r1.height, 2)
+        r1.update(89, 1, 2, 3)
+        self.assertEqual(r1.x, 3)
+        r1.update(89, 1, 2, 3, 4)
+        self.assertEqual(r1.y, 4)
+        r2 = Rectangle(1, 1)
+        r2.update(**{ 'id': 89 })
+        self.assertEqual(r2.id, 89)
+        r2.update(**{ 'id': 89, 'width': 1 })
+        self.assertEqual(r2.width, 1)
+        r2.update(**{ 'id': 89, 'width': 1, 'height': 2 })
+        self.assertEqual(r2.height, 2)
+        r2.update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 })
+        self.assertEqual(r2.x, 3)
+        r2.update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 })
+        self.assertEqual(r2.y, 4)
+    def test_rectangle_create(self):
+        r1 = Rectangle.create(**{ 'id': 89 })
+        self.assertEqual(r1.id, 89)
+        r1 = Rectangle.create(**{ 'id': 89, 'width': 1 })
+        self.assertEqual(r1.width, 1)
+        r1 = Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2 })
+        self.assertEqual(r1.height, 2)
+        r1 = Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 })
+        self.assertEqual(r1.x, 3)
+        r1 = Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 })
+        self.assertEqual(r1.y, 4)
+    def test_rectangle_save_to_file(self):
+        Rectangle.save_to_file(None)
+        if not os.path.isfile("Rectangle.json"):
+            raise AssertionError()
+        Rectangle.save_to_file([])
+        if not os.path.isfile("Rectangle.json"):
+            raise AssertionError()
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        if not os.path.isfile("Rectangle.json"):
+            raise AssertionError()
+    def test_rectangle_load_from_file(self):
+        """
+        loaded = Rectangle.load_from_file()
+        self.assertEqual(loaded, [])
+        Rectangle.save_to_file(Rectangle(1, 1, 1, 1, 1))
+        loaded = Rectangle.load_from_file()
+        self.assertEqual(loaded, {'x': 1, 'y': 1, 'id': 1, 'height': 1, 'width': 1})
+        """
+        pass
