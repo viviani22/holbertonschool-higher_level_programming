346a6cb662539ec921ac498f3c430fe410f63844
viviani22 <vivianxhumetiku@gmail.com>
2022-10-06 01:22:42 -0700

    add tests

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,25 @@
+import unittest
+from models.rectangle import Rectangle
+
+class TestBase(unittest.TestCase):
+    def test_rectangle_assignments(self):
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        r2 = Rectangle(1, 2, 3)
+        r3 = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(TypeError):
+            r4 = Rectangle("1", 2)
+            r4 = Rectangle(1, "2")
+            r2 = Rectangle(1, 2, "3")
+            r3 = Rectangle(1, 2, 3, "4")
+        r3 = Rectangle(1, 2, 3, 4, 5)
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-1, 2)
+            r1 = Rectangle(1, -2)
+            r1 = Rectangle(0, 2)
+            r1 = Rectangle(1, 0)
+            r1 = Rectangle(1, 2, -3)
+            r1 = Rectangle(1, 2, 3 ,-4)
+    def test_rectangle_methods(self):
+        pass
