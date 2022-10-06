c7ec764a699b5e432c0792aac624341f3d503273
viviani22 <vivianxhumetiku@gmail.com>
2022-10-06 01:28:33 -0700

    up

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -17,12 +17,18 @@ class TestBase(unittest.TestCase):
         with self.assertRaises(TypeError):
             r3 = Rectangle(1, 2, 3, "4")
         r4 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r4.id, 5)
         with self.assertRaises(ValueError):
             r1 = Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
             r1 = Rectangle(1, -2)
+        with self.assertRaises(ValueError):
             r1 = Rectangle(0, 2)
+        with self.assertRaises(ValueError):
             r1 = Rectangle(1, 0)
+        with self.assertRaises(ValueError):
             r1 = Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
             r1 = Rectangle(1, 2, 3 ,-4)
     def test_rectangle_methods(self):
         pass
