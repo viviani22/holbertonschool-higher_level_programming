a437ea0c7991c44fc9898b2bfecc486d0d85fe26
viviani22 <vivianxhumetiku@gmail.com>
2022-10-07 00:59:32 -0700

    task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,7 +40,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        dummy = cls(1, 1)
+        dummy = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
         dummy.update(**dictionary)
         return dummy
 
