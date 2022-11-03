2f190da4f1b21285035bc960f2cc0259e40683d0
viviani22 <vivianxhumetiku@gmail.com>
2022-11-02 23:38:17 -0700

    task 17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- Converts DB to UTF-8 incoding
-USE hbtn_0c_0 ALTER TABLE CONVERT TO first_table CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
