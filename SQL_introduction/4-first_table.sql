3a815cd74a66016e40366254c6df440500f14d7e
viviani22 <vivianxhumetiku@gmail.com>
2022-11-02 22:49:49 -0700

    fix spelling

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table called first_table in current DB
-CREATE TABLE IF NOT EXISTS first_tabl (
+CREATE TABLE IF NOT EXISTS first_table (
 	`id` INT,
 	`name` VARCHAR(256)
 );
