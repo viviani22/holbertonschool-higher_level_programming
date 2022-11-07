5ea07f05072c7b5b2a09412c5fabf9c10d20169d
viviani22 <vivianxhumetiku@gmail.com>
2022-11-07 00:28:37 -0800

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states 
+WHERE states.name = 'California'
 ORDER BY cities.id ASC;
