c8c4d27751cf58064788e3144b177aaa37049191
viviani22 <vivianxhumetiku@gmail.com>
2022-11-08 02:19:40 -0800

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Task 0 """
+""" lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -11,3 +11,5 @@ cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 rows = cur.fetchall()
 for row in rows:
     print(row)
+cur.close()
+db.close()
