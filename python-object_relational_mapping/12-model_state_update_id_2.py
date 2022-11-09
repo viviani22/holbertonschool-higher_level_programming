8f0e52d11ae8f11bfa8c17b63bc2c54e0dfb499c
viviani22 <vivianxhumetiku@gmail.com>
2022-11-08 22:41:55 -0800

    task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument """
+from model_state import Base, State
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
+                           f"@localhost/{sys.argv[3]}")
+    with Session(engine) as session:
+        session.query(State).filter(State.id == 2).\
+                update({State.name: "New Mexico"}, synchronize_session='fetch')
+        session.commit()
