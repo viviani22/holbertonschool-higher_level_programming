a1bbe0a3932a31b4babc82d826bc7d90eafb2061
viviani22 <vivianxhumetiku@gmail.com>
2022-12-06 04:27:11 -0800

    task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
