17691a3ee1445ebe99e0078c9132644e71089e7d
viviani22 <vivianxhumetiku@gmail.com>
2022-12-05 22:57:42 -0800

    task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+import process from 'node:process';
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
