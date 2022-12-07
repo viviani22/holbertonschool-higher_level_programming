2b539ede8b067cf67558c0a622e46b4bfa5b3f58
viviani22 <vivianxhumetiku@gmail.com>
2022-12-07 00:14:18 -0800

    fix 4 checker

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 import process from 'node:process';
 const myArgs = process.argv.slice(2);
-if (myArgs[0] !== undefined) {
+if (isNaN(myArgs[0]) !== true) {
   const myNum = parseInt(myArgs[0]);
   for (let i = 0; i < myNum; i++) {
     console.log('C is fun');
