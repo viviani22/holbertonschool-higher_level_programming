5484696cbf62b527e7577927197329ea84eac20a
viviani22 <vivianxhumetiku@gmail.com>
2022-12-07 00:20:19 -0800

    og

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 import process from 'node:process';
 const myArgs = process.argv.slice(2);
-if (isNaN(myArgs[0]) !== true) {
+if (myArgs[0] !== undefined) {
   const myNum = parseInt(myArgs[0]);
   for (let i = 0; i < myNum; i++) {
     console.log('C is fun');
