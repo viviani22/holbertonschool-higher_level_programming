3622c3200decf6bb7a97e77b6751306ff539a080
viviani22 <vivianxhumetiku@gmail.com>
2022-12-13 09:45:31 -0800

    task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+let i = 0;
+
+request('https://swapi-api.hbtn.io/api/films/', function (error, response, body) {
+  let number = 0;
+  const dict = JSON.parse(body);
+  for (const [key, value] of Object.entries(dict.results)) {
+    for (let j = 0; j < (dict.results[i].characters).length; j++) {
+      if (dict.results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+      console.log(dict.results[i].characters[j]);
+	      number += 1;
+      }
+    }
+    i += 1;
+  }
+console.log(number);
+});
