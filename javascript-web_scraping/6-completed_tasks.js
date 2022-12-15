4e52b1ce91afa7bb04c1546f445f93cc8a7c6ab5
viviani22 <vivianxhumetiku@gmail.com>
2022-12-14 22:12:28 -0800

    fix task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,22 +2,15 @@
 
 const request = require('request');
 request('https://jsonplaceholder.typicode.com/todos', function(error, response, body) {
-  let dict = JSON.parse(body);
-  let userID = 1;
-  let nr_tasks = {};
-  for (let item of dict) {
-    if (item.userId === userID) {
-      if (userID in nr_tasks && item.completed) {
-        nr_tasks[userID] += 1;
+  const dict = JSON.parse(body);
+  const nr_tasks = {};
+  for (const user in dict) {
+    if (dict[user].completed) {
+      if (nr_tasks[dict[user].userId] === undefined) {
+        nr_tasks[dict[user].userId] = 1;
+      } else {
+        nr_tasks[dict[user].userId] += 1;
       }
-      else {
-	if (item.completed) {
-          nr_tasks[userID] = 1;
-	}
-      }
-    }
-    else {
-      userID += 1;
     }
   }
   console.log(nr_tasks);
